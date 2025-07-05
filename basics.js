5efd7997ac861243329cfa945d61d137005a0d36
Shresth Tyagi <shresthtyagi1996@gmail.com>
2025-07-05 12:12:21 +0530

    Changed the basics.js file to change font size by points in place of pixels

--- basics.js
+++ basics.js
@@ -2,6 +2,6 @@ let para = document.getElementById("paragraph");
 let range = document.getElementById("fontSize");
 
 range.addEventListener("input", function () {
-  let fontSize = range.value + "px";
+  let fontSize = range.value + "pt";
   para.style.fontSize = fontSize;
 });
