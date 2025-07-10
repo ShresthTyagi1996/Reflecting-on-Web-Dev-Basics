c360cdf7f9be447c70699b36dbee5048fb3a351d
Shresth Tyagi <shresthtyagi1996@gmail.com>
2025-07-10 20:36:11 +0530

    Using GitBash

--- basics.js
+++ basics.js
@@ -5,3 +5,8 @@ range.addEventListener("input", function () {
   let fontSize = range.value + "pt";
   para.style.fontSize = fontSize;
 });
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY;
+  this.document.body.classList.toggle("scrolled", scrollTop > 0);
+});
